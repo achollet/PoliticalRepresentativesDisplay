3fcebe25e6593b529fb0e87d3d6c3ebeebb61d7a
CHOLLET-EXT Alexis <alexis.chollet-ext@fnacdarty.com>
2017-09-01 14:34:48 +0200

    Filling seed files

--- Base.DAL/Context/GeographyContext.cs
+++ Base.DAL/Context/GeographyContext.cs
@@ -9,6 +9,7 @@ namespace Base.DAL.Context
         public DbSet<Region> Regions { get; set; }
         public DbSet<Department> Departments { get; set; }
         public DbSet<District> Districts { get; set; }
+        public DbSet<Constituency> Constituencies { get; set; }
         public DbSet<Town> Towns { get; set; }
         public DbSet<TownType> TownTypes { get; set; }
 
@@ -18,6 +19,7 @@ namespace Base.DAL.Context
             modelBuilder.Entity<Region>().ToTable("Region");
             modelBuilder.Entity<Department>().ToTable("Department");
             modelBuilder.Entity<District>().ToTable("District");
+            modelBuilder.Entity<Constituency>().ToTable("Constituency");
             modelBuilder.Entity<Town>().ToTable("Town");
             modelBuilder.Entity<TownType>().ToTable("TownType");
         }
--- Base.DAL/Entities/Geography/Department.cs
+++ Base.DAL/Entities/Geography/Department.cs
@@ -11,9 +11,12 @@ namespace Base.DAL.Entities
         public int Id { get; set; }
 
         [Required]
-        [MaxLength(5)]
-        public string Code { get; set; }
+        [MaxLength(6)]
+        public string CodeIso { get; set; }
         
+        [MaxLength(3)]
+        public string CodeInsee { get; set; }
+
         [Required]
         [MaxLength(250)]
         public string Name { get; set; }
@@ -22,6 +25,7 @@ namespace Base.DAL.Entities
         [ForeignKey("RegionId")]
         public int RegionId { get; set; }
         public IEnumerable<District> Districts { get; set; }
+        public IEnumerable<Constituency> Constituencies { get; set; }
         public IEnumerable<Town> Towns { get; set; }
     }
 }
